2f23ddb3398bd99411817aaf81ea81610ab92c77
orlandouchiha425 <valadez425@gmail.com>
2022-04-06 19:28:41 -0400

    changes to Main-App

--- main-app/server.js
+++ main-app/server.js
@@ -8,5 +8,24 @@ app.get('/',(req,res)=>{
     res.send(`<h1>Main App</a>`)
 
 })
+///https://betterprogramming.pub/fibonacci-algorithm-in-javascript-45743f3a0ff6 
+///I Have no idea about finobacci--- 
+app.get('/:id',(req,res)=>{
+    function fibonacci(element) {
+        const sequence = [0, 1];
+        for (i = 2; i <= element; i++) {
+            sequence[i] = sequence[i - 2] + sequence[i - 1];
+        }
+        return sequence[element];
+    }
+    res.send(fibonacci())
+
+})
+
+
+
 
 app.listen(port, () => console.log(`Now Listening on port ${port}`));
+
+
+
